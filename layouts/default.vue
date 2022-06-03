3d381f97edb2ae0bd251beef972dd1f32b2c3243
Bayu K <bayukembara@gmail.com>
2022-06-04 01:17:35 +0700

    footer template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,20 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer v-model="sideDrawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+    <v-navigation-drawer
+      v-model="sideDrawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
       <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+        <v-list-item
+          v-for="(item, i) in sideMenu"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -13,59 +25,80 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="sideDrawer = !sideDrawer" />
       <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
+
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+
+    <v-bottom-navigation horizontal height="10hv" fixed color="primary" app>
+      <v-app-bar-nav-icon
+        @click.stop="sideDrawer = !sideDrawer"
+        v-ripple="false"
+        plain
+      />
+      <v-btn
+        v-for="(item, i) in bottomMenu"
+        :key="i"
+        :to="item.to"
+        v-ripple="false"
+        plain
+      >
+        <span>{{ item.title }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+    <v-spacer />
   </v-app>
 </template>
 
 <script>
-  export default {
-    name: 'DefaultLayout',
-    data() {
-      return {
-        sideDrawer: false,
-        items: [{
-            icon: 'mdi-account',
-            title: 'Welcome',
-            to: '/'
-          },
-          {
-            icon: 'mdi-chart-bubble',
-            title: 'Inspire',
-            to: '/inspire'
-          }
-        ],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
-      }
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      sideDrawer: false,
+      sideMenu: [
+        {
+          icon: 'mdi-application',
+          title: 'App',
+          to: '/',
+        },
+        {
+          icon: 'mdi-account',
+          title: 'Account',
+          to: '/account',
+        },
+        {
+          icon: 'mdi-bell',
+          title: 'Notification',
+          to: '/notification',
+        },
+      ],
+      bottomMenu: [
+        {
+          icon: 'mdi-application',
+          title: 'App',
+          to: '/',
+        },
+        {
+          icon: 'mdi-account',
+          title: 'Account',
+          to: '/account',
+        },
+        {
+          icon: 'mdi-bell',
+          title: 'Notification',
+          to: '/notification',
+        },
+      ],
+      right: true,
+      rightDrawer: false,
+      title: 'Cashier',
     }
-  }
-
+  },
+}
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,46 +5,59 @@
         <NuxtLogo />
         <VuetifyLogo />
       </v-card>
-      <v-card>s
+      <v-card>
         <v-card-title class="headline">
           Welcome to the Vuetify + Nuxt.js template
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
           <p>
-            For more information on Vuetify, check out the <a
+            Vuetify is a progressive Material Design component framework for
+            Vue.js. It was designed to empower developers to create amazing
+            applications.
+          </p>
+          <p>
+            For more information on Vuetify, check out the
+            <a
               href="https://vuetifyjs.com"
               target="_blank"
               rel="noopener noreferrer"
             >
               documentation
-            </a>.
+            </a>
+            .
           </p>
           <p>
-            If you have questions, please join the official <a
+            If you have questions, please join the official
+            <a
               href="https://chat.vuetifyjs.com/"
               target="_blank"
               rel="noopener noreferrer"
               title="chat"
             >
               discord
-            </a>.
+            </a>
+            .
           </p>
           <p>
-            Find a bug? Report it on the github <a
+            Find a bug? Report it on the github
+            <a
               href="https://github.com/vuetifyjs/vuetify/issues"
               target="_blank"
               rel="noopener noreferrer"
               title="contribute"
             >
               issue board
-            </a>.
+            </a>
+            .
           </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <p>
+            Thank you for developing with Vuetify and I look forward to bringing
+            more exciting features in the future.
+          </p>
           <div class="text-xs-right">
             <em><small>&mdash; John Leider</small></em>
           </div>
-          <hr class="my-3">
+          <hr class="my-3" />
           <a
             href="https://nuxtjs.org/"
             target="_blank"
@@ -52,7 +65,7 @@
           >
             Nuxt Documentation
           </a>
-          <br>
+          <br />
           <a
             href="https://github.com/nuxt/nuxt.js"
             target="_blank"
@@ -63,11 +76,7 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
+          <v-btn color="primary" nuxt to="/inspire">
             Continue
           </v-btn>
         </v-card-actions>
@@ -78,6 +87,6 @@
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
 }
 </script>
